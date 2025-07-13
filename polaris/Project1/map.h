f1a638987ef45a5cf29d58d2d55f8f0b88dc31d9
milky6394 <takaokayu@outlook.jp>
2025-07-13 22:21:03 +0900

    map.h実装完了

--- polaris/Project1/map.h
+++ polaris/Project1/map.h
@@ -2,8 +2,11 @@
 #include "func.h"
 
 
-int Mappixel(int x) {
+int Mappixelx(int x) {
     return x * 100;
+}
+int Mappixely(int y) {
+    return y * 100;
 }
 
 class MapControler {
@@ -11,15 +14,15 @@ public:
     int map[10][10] = {};//map‚ðŠi”[‚·‚é”z—ñ
 
     MapControler() {
-        map[9][9] = 1;
+        map[1][1] = 1;
     }
 
 
     void MapPracer() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (map[i][j] = 5) {
-                    DrawCircle(Mappixel(i), Mappixel(j), 10, GetColor(255, 255, 255), TRUE);
+                if (map[i][j] == 1) {
+                    DrawCircle(Mappixelx(i), Mappixely(j), 10, GetColor(255, 255, 255), TRUE);
                 }
             }
         }

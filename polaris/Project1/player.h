0cb200088319725b9325ed6eca2e6c34c887fe1d
milky6394 <takaokayu@outlook.jp>
2025-07-13 20:49:04 +0900

    playerのエラー修正

--- polaris/Project1/player.h
+++ polaris/Project1/player.h
@@ -1,5 +1,75 @@
 #include "DxLib.h"
 #include "func.h"
+bool pushspace = false, pushup = false, pushdown = false, pushright = false, pushleft = false;//各ボタンを長押しできないようにするための変数
+bool Space() {
+    if (CheckHitKey(KEY_INPUT_SPACE)) {
+        if (!pushspace) {
+            pushspace = true;
+            return true;
+        }
+    }
+    else {
+        pushspace = false;
+    }
+    return false;
+}
+//spaceキー1回押しの関数
+
+bool Up() {
+    if (CheckHitKey(KEY_INPUT_UP)) {
+        if (!pushup) {
+            pushup = true;
+            return true;
+        }
+    }
+    else {
+        pushup = false;
+    }
+    return false;
+}
+//upキー1回押しの関数
+
+bool Down() {
+    if (CheckHitKey(KEY_INPUT_DOWN)) {
+        if (!pushdown) {
+            pushdown = true;
+            return true;
+        }
+    }
+    else {
+        pushdown = false;
+    }
+    return false;
+}
+//downキー1回押しの関数
+
+bool Right() {
+    if (CheckHitKey(KEY_INPUT_RIGHT)) {
+        if (!pushright) {
+            pushright = true;
+            return true;
+        }
+    }
+    else {
+        pushright = false;
+    }
+    return false;
+}
+//rightキー1回押しの関数
+
+bool Left() {
+    if (CheckHitKey(KEY_INPUT_LEFT)) {
+        if (!pushleft) {
+            pushleft = true;
+            return true;
+        }
+    }
+    else {
+        pushleft = false;
+    }
+    return false;
+}
+//leftキー1回押しの関数
 
 class Player {
 public:

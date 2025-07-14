540b663367f47d544b2a0ca376ae380c53dc88db
milky6394 <takaokayu@outlook.jp>
2025-07-15 01:18:31 +0900

    eventのZボタン実装

--- polaris/Project1/event.h
+++ polaris/Project1/event.h
@@ -3,56 +3,22 @@
 class EventControler {
 public:
     int x, y;
+    bool pushz = false;
+
     EventControler() {
         x = 1000;
         y = 400;
     }
 
-    bool Up() {
-        if (CheckHitKey(KEY_INPUT_UP)) {
-            if (!pushup) {
-                pushup = true;
+    bool Button_Z() {
+        if (CheckHitKey(KEY_INPUT_Z)) {
+            if (!pushz) {
+                pushz = true;
                 return true;
             }
         }
         else {
-            pushup = false;
-        }
-        return false;
-    }
-    bool Down() {
-        if (CheckHitKey(KEY_INPUT_DOWN)) {
-            if (!pushdown) {
-                pushdown = true;
-                return true;
-            }
-        }
-        else {
-            pushdown = false;
-        }
-        return false;
-    }
-    bool Right() {
-        if (CheckHitKey(KEY_INPUT_RIGHT)) {
-            if (!pushright) {
-                pushright = true;
-                return true;
-            }
-        }
-        else {
-            pushright = false;
-        }
-        return false;
-    }
-    bool Left() {
-        if (CheckHitKey(KEY_INPUT_LEFT)) {
-            if (!pushleft) {
-                pushleft = true;
-                return true;
-            }
-        }
-        else {
-            pushleft = false;
+            pushz = false;
         }
         return false;
     }

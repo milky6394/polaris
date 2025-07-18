00cbd680d7a24db4909fd88ef8d7b32d0ba4df93
milky6394 <takaokayu@outlook.jp>
2025-07-18 21:31:59 +0900

    item.h追加

--- polaris/Project1/event.h
+++ polaris/Project1/event.h
@@ -1,12 +1,13 @@
 #include "DxLib.h"
+#include "item.h"
 
 class EventControler {
 public:
     int x, y;
     int st;
-    char String[100] = { "今日の天気は曇りなんだけどこっから先はどんな感じに見えるのかが気になるのでたくさん書いてみます" };
+    char String[1000] = { "今日の天気は曇りなんだけどこっから先はどんな感じに見えるのかが気になるのでたくさん書いてみます" };
 
-    char NowSt[100] = {};
+    char NowSt[1000] = {};
 
     EventControler() {
         x = 1000;
@@ -15,17 +16,17 @@ public:
     }
 
     void StringReset() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             NowSt[i] = {};
         }
     }
 
     void EventDraw() {
         NowSt[st] = String[st];
-        if (st < 99) {
+        if (st < 999) {
             st++;
         }
-        DrawFormatString(300, 600, GetColor(255, 255, 255), " %s ", NowSt);
+        DrawFormatString(300, 600, GetColor(255, 255, 255), "%s", NowSt);
         DrawTriangle(x, y, x + 10, y - 10, x - 10, y - 10, GetColor(255, 255, 255), true);
     }
 };

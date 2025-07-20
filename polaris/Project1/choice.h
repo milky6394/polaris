aa37839d3dbc037f58ad2322ebc5d1f9c9781361
milky6394 <takaokayu@outlook.jp>
2025-07-20 21:40:23 +0900

    choice.h追加

---
+++ polaris/Project1/choice.h
@@ -0,0 +1,32 @@
+#include "DxLib.h"
+
+class ChoiceManager {
+public:
+    ItemManager* im = new ItemManager();
+
+    int x, y;
+    int st;
+    char String[1000] = {};
+
+    ChoiceManager() {
+        x = 1000;
+        y = 650;
+        st = 0;
+    }
+
+    void StringReset() {
+        for (int i = 0; i < 1000; i++) {
+            String[i] = {};
+        }
+    }
+    //String‚ð‰Šú‰»‚·‚éŠÖ”
+
+    void EventDraw(int a) {
+        String[st] = im->ItemString[a][st];
+        if (st < 999) {
+            st++;
+        }
+        DrawFormatString(300, 600, GetColor(255, 255, 255), "%s", String);
+        DrawTriangle(x, y, x + 10, y - 10, x - 10, y - 10, GetColor(255, 255, 255), true);
+    }
+};
